8232b52e7bd1c5a6dd3d95f2f288ec51e8f4a8d9
Ronjie Diafante Man-on <rdmanon@vauldex.com>
2025-04-28 14:25:42 +0800

    - created Main Views and Sub Views - added BackgroundImageView

---
+++ FizzBuzz/View/Sub Views/BackgroundImageView.swift
@@ -0,0 +1,18 @@
+//
+//  BackgroundImageView.swift
+//  FizzBuzz
+//
+//  Created by Ronjie Diafante Man-on on 4/28/25.
+//
+
+import SwiftUI
+
+struct BackgroundImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BackgroundImageView()
+}

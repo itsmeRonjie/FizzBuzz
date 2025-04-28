c0927f2551d969588e59efe33d5638837cd8d3bc
Ronjie Diafante Man-on <rdmanon@vauldex.com>
2025-04-28 13:08:59 +0800

    initialized project

---
+++ FizzBuzz/FizzBuzzApp.swift
@@ -0,0 +1,17 @@
+//
+//  FizzBuzzApp.swift
+//  FizzBuzz
+//
+//  Created by Ronjie Diafante Man-on on 4/28/25.
+//
+
+import SwiftUI
+
+@main
+struct FizzBuzzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
